49d4a4b6f67910060e2ec951f6cf72eeebe4d326
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-08-11 11:56:41 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad=0;
+            string cadena = "";
+            Type type;
+
+
+
+            type = edad.GetType();
+            mostrarTipo(type);
+            type = cadena.GetType();
+            mostrarTipo(type);
+
+            Personas personas = new Personas();
+            //type = personas.GetType();
+            
+
+            
+            type = typeof(Personas);
+
+            PropertyInfo[] propertyInfo = type.GetProperties();
+            foreach (PropertyInfo pInfo in propertyInfo)
+            {
+                Console.WriteLine(pInfo.Name);
+            }
+
+
+            var metodos = type.GetMethods();
+            foreach (var item in metodos)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            Console.ReadLine();
+        }
+        public static void mostrarTipo(Type type)
+        {
+            Console.WriteLine(type);
+        }
+    }
+}
