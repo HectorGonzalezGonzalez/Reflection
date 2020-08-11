d228858b985c06e122308dfb4e15d441ccd90b33
HectorGonzalezGonzalez <37567557+HectorGonzalezGonzalez@users.noreply.github.com>
2020-08-11 12:01:29 -0500

    comentario actualizado

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using System.Reflection;//Espacio de nombres utilizado
 using System.Text;
 using System.Threading.Tasks;
 
